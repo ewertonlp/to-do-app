7e6fa114642886860c108383000609636052971f
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2022-04-09 17:04:07 -0300

    Normalização e Validação dos Forms Login e Cadastro.

---
+++ scripts/cadastro.js
@@ -0,0 +1,215 @@
+// Normalização da tela de Cadastro de usuário
+const form = document.querySelector('#form-cadastro');
+const nome = document.querySelector('#inputNome');
+const labelNome = document.querySelector('#labelNome');
+
+const sobrenome = document.querySelector('#inputSobrenome');
+const labelSobrenome = document.querySelector('#labelSobrenome');
+
+const email = document.querySelector('#inputEmail');
+const labelEmail = document.querySelector('#labelEmail');
+
+const password = document.querySelector('#inputSenha');
+const labelSenha = document.querySelector('#labelSenha');
+
+const passwordConfirmaton = document.querySelector('#inputRepetirSenha');
+const labelRepetirSenha = document.querySelector('#labelRepetirSenha');
+
+
+const btnCadastro = document.querySelector('#btn-cadastro');
+const btnVerSenha = document.querySelector("#verSenha");
+const btnVerConfirmSenha = document.querySelector('#verConfirmSenha');
+
+const regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const msgError = document.querySelector("#msgError");
+const msgSuccess = document.querySelector("#msgSuccess");
+
+const cadastroURL = 'https://ctd-todo-api.herokuapp.com/v1/users/';
+
+
+
+// Função para Validação dos Inputs
+btnCadastro.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (nome.value != "" && sobrenome.value != "" && email.value != "" && password.value != "" &&
+        passwordConfirmaton.value != "") {
+
+
+        // Criamos objeto para receber as variáveis de cadastro.
+        const data = {
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            email: email.value,
+            password: password.value,
+            passwordConfirmaton: passwordConfirmaton.value
+        }
+
+        const settings = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+
+            body:
+
+                JSON.stringify(data),
+        };
+
+        // Chamando   a API
+        fetch(cadastroURL, settings)
+
+            .then((response) => {
+                return response.json();
+            })
+            .then(function (resposta) {
+                cadastroSucesso(nome.value, sobrenome.value, email.value, resposta.jwt)
+            })
+            .catch(error => {
+                cadastroErro(error)
+            });
+
+
+
+    } else {
+        // Exibe mensagem quando houver erro nos campos Input
+        msgError.setAttribute('style', 'display:block');
+        msgError.innerHTML = '<strong>Erro ao cadastrar usuário, tente novamente!</strong>'
+        msgSuccess.innerHTML = ''
+        msgSuccess.setAttribute('style', 'display:none')
+    }
+
+
+    /*  Ao obter o sucesso, recebe o json (token) do usuário*/
+    function cadastroSucesso(nomeUsuario, sobrenomeUsuario, emailUsuario, jsonRecebido) {
+
+        // Atribui os dados ao localStorage com o Nome do User, Sobrenome e o Token
+        localStorage.setItem("user", JSON.stringify({
+            nome: nomeUsuario,
+            sobrenome: sobrenomeUsuario,
+            email: emailUsuario,
+            token: jsonRecebido
+        }))
+
+        // Exibe mensagem de sucesso
+        msgSuccess.setAttribute('style', 'display:block');
+        msgSuccess.innerHTML = '<strong>Usuário cadastrado com sucesso.</strong>'
+        msgError.setAttribute('style', 'display:none')
+        msgError.innerHTML = ''
+
+        // Se todos os campos estiverem OK, então redirecionamos para a pagina de tarefas;
+        setTimeout(() => {
+            window.location.href = "tarefas.html"
+        }, 3000)
+    }
+
+
+    // Caso ocorra algum erro, mostra o erro no console
+    function cadastroErro(statusRecebido) {
+
+        console.log("Erro ao cadastrar");
+        console.log(statusRecebido)
+    }
+});
+
+
+
+
+// ------------ Normalização dos campos Input  ------------------//  
+nome.addEventListener('keyup', () => {
+
+    if (nome.value === "") {
+        console.log(nome)
+        labelNome.setAttribute('style', 'color: #D85A5A');
+        labelNome.innerHTML = '<strong>Nome (Este campo não pode estar vazio)</strong>'
+        nome.setAttribute('style', 'border-color: #D85A5A');
+    } else {
+        labelNome.setAttribute('style', 'color: #4ECa64');
+        labelNome.innerHTML = 'Nome';
+        nome.setAttribute('style', 'border-color: #4ECa64');
+
+    }
+});
+
+sobrenome.addEventListener('keyup', () => {
+
+    if (sobrenome.value === "") {
+        labelSobrenome.setAttribute('style', 'color: #D85A5A');
+        labelSobrenome.innerHTML = '<strong>Sobrenome (Este campo não pode estar vazio)</strong>'
+        sobrenome.setAttribute('style', 'border-color: #D85A5A');
+    } else {
+        labelSobrenome.setAttribute('style', 'color: #4ECa64');
+        labelSobrenome.innerHTML = 'Sobrenome:';
+        sobrenome.setAttribute('style', 'border-color: #4ECa64');
+
+    }
+});
+
+email.addEventListener('keyup', () => {
+
+    // let inputEmail = email.value.toLowerCase();
+    // email.value = inputEmail;
+
+    if (regexMail.test(email.value) == false) {
+        labelEmail.setAttribute('style', 'color: #D85A5A');
+        labelEmail.innerHTML = '<strong>Email (email inválido ou campo vazio)</strong>'
+        email.setAttribute('style', 'border-color: #D85A5A');
+    } else {
+        labelEmail.setAttribute('style', 'color: #4ECa64');
+        labelEmail.innerHTML = 'Email:';
+        email.setAttribute('style', 'border-color: #4ECa64');
+
+    }
+});
+
+
+password.addEventListener('keyup', () => {
+
+    if (password.value.length < 8 || password.value.length > 12 || password === "") {
+        labelSenha.setAttribute('style', 'color: #D85A5A');
+        labelSenha.innerHTML = '<strong>Senha deve conter entre 8 e 12 caracteres</strong>'
+        password.setAttribute('style', 'border-color: #D85A5A');
+    } else {
+        labelSenha.setAttribute('style', 'color: #4ECa64');
+        labelSenha.innerHTML = 'Senha:';
+        password.setAttribute('style', 'border-color: #4ECa64');
+
+    }
+});
+
+passwordConfirmaton.addEventListener('keyup', () => {
+
+    if (passwordConfirmaton.value != password.value || passwordConfirmaton.value == "") {
+        labelRepetirSenha.setAttribute('style', 'color: #D85A5A');
+        labelRepetirSenha.innerHTML = '<strong>As senhas não conferem</strong>'
+        passwordConfirmaton.setAttribute('style', 'border-color: #D85A5A');
+    } else {
+        labelRepetirSenha.setAttribute('style', 'color: #4ECa64');
+        labelRepetirSenha.innerHTML = 'Confirme sua Senha:';
+        passwordConfirmaton.setAttribute('style', 'border-color: #4ECa64');
+
+    }
+});
+
+
+
+// ---------- Lógica para  mostrar e esconder senha ------------- //
+btnVerSenha.addEventListener('click', () => {
+    let inputPassword = document.querySelector('#inputSenha');
+
+    if (inputPassword.getAttribute('type') == 'password') {
+        inputPassword.setAttribute('type', 'text')
+    } else {
+        inputPassword.setAttribute('type', 'password')
+    }
+})
+
+btnVerConfirmSenha.addEventListener('click', () => {
+    let inputPassword = document.querySelector('#inputRepetirSenha');
+    if (inputPassword.getAttribute('type') == 'password') {
+        inputPassword.setAttribute('type', 'text')
+    } else {
+        inputPassword.setAttribute('type', 'password')
+    }
+})
---
+++ scripts/login.js
@@ -0,0 +1,143 @@
+// Reservando as informações dos inputs em variáveis.
+const inputEmail = document.getElementById('inputEmail');
+const labelEmail = document.querySelector('#labelEmail');
+const inputPassword = document.getElementById('inputPassword');
+const labelSenha = document.querySelector('#labelSenha');
+const btnVerSenha = document.querySelector("#verSenha");
+const regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+// Trazer URL base da API com endpoint Login
+const loginUrl = 'https://ctd-todo-api.herokuapp.com/v1/users/login';
+
+
+// Função utilizada ao clicar no botão
+function login() {
+
+    // normalizamos o campo de email, recebendo o que é digitado e armazenamos na variavel email.
+    let email = inputEmail.value.toLowerCase();
+    inputEmail.value = email;
+
+    // o mesmo é feito para a variavel senha. 
+    let senha = inputPassword.value
+
+
+    // se o email digitado passar pelo teste do regex, o mesmo é armazenado na localStorage.
+    if (regexMail.test(email)) {
+        localStorage.setItem('login', JSON.stringify({ email: email }));
+
+
+        // Aqui passamos então um objeto, que receberá a variavel email e senha como valor para os dois campos obrigatórios para nosso body.
+        const data = {
+            email: email,
+            password: senha,
+        };
+
+
+        // neste console log trazemos então para o  data, o email e senha do usuario.
+        console.log(data);
+
+        // criamos uma variavel para armazenar as configuracoes da API e do endpoint , como o seu metodo e o seu header.
+        const settings = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+
+            // então chamamos a variavel data que recebeu os valores dos campos, e aplicamos o JSON stringfy para tornar o objeto um JSON.
+            body:
+
+                JSON.stringify(data),
+        };
+
+
+        // passamos então como parametro a variavel que contem a URL, e a variavel que contem as configurações
+        fetch(loginUrl, settings)
+
+            // aqui usamos response para validar se positiva, retornamos a response.json
+            .then(response => {
+                if (response.status === 201) {
+
+                    console.log(response)
+                    return response.json()
+                }
+
+                // se não enviado para o catch e apresentado erro de login.
+                throw response;
+            })
+
+            // com a resposta usamos uma simples funcao para armazenar o JWT no localStorage, como é proposto pelo exercicio.
+
+            .then(function (resposta) {
+
+                // neste console podemos ver como resposta o JWT que é gerado ao logar com sucesso.
+                console.log(resposta)
+                alert("Login efetuado com sucesso!")
+                loginSucesso(resposta.jwt)
+                // location.href = 'tarefas.html';
+
+            })
+            .catch(err => {
+                console.log(err);
+                alert("Falha no login!")
+            });
+
+        // temos então um else para caso reprovado no test do regex, ele apresente a mensagem de formato invalido.
+    } else {
+        alert("Formato de email invalido")
+    }
+
+    function loginSucesso(jwtRecebido) {
+
+        console.log(jwtRecebido);
+
+        // Após obter o jwt, salva no localStorage ou SessionStorage
+        localStorage.setItem("jwt", jwtRecebido);
+
+        // Direciona o usuário para a tela de tarefas após sucesso ao logar
+        window.location.href = "tarefas.html"
+    }
+
+}
+
+// ----------- Normalização dos campos Email e Senha ------- //
+inputEmail.addEventListener('keyup', (e) => {
+
+    if (regexMail.test(inputEmail.value) == false) {
+        labelEmail.setAttribute('style', 'color: #D85A5A');
+        labelEmail.innerHTML = '<strong>Email (email inválido ou campo vazio)</strong>'
+        inputEmail.setAttribute('style', 'border-color: #D85A5A');
+    } else {
+        labelEmail.setAttribute('style', 'color: #4ECa64');
+        labelEmail.innerHTML = 'Email:';
+        inputEmail.setAttribute('style', 'border-color: #4ECa64');
+
+    }
+});
+
+
+inputPassword.addEventListener('keyup', () => {
+
+    if (inputPassword.value.length < 8 || inputPassword.value.length > 12 || inputPassword === "") {
+        labelSenha.setAttribute('style', 'color: #D85A5A');
+        labelSenha.innerHTML = '<strong>Senha deve conter entre 8 e 12 caracteres</strong>'
+        inputPassword.setAttribute('style', 'border-color: #D85A5A');
+    } else {
+        labelSenha.setAttribute('style', 'color: #4ECa64');
+        labelSenha.innerHTML = 'Senha:';
+        inputPassword.setAttribute('style', 'border-color: #4ECa64');
+
+    }
+});
+
+
+
+// ---------- Lógica para  mostrar e esconder senha ------------- //
+btnVerSenha.addEventListener('click', () => {
+    let inputPassword = document.querySelector('#inputSenha');
+
+    if (inputPassword.getAttribute('type') == 'password') {
+        inputPassword.setAttribute('type', 'text')
+    } else {
+        inputPassword.setAttribute('type', 'password')
+    }
+})
