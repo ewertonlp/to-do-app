9b5dc5cad6fa7a15c4a961492059574c66b1dfc1
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2022-04-13 16:44:08 -0300

    acessar tarefas.html, listar e incluir novas tarefas.

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,3 +1,16 @@
+//Variável para guardar tag de nome do usuário
+const nomeUsuario = querySelector('.user-info p');
+
+//link de integração com API - recuperar dados do usuário
+const urlGetMe = 'https://ctd-todo-api.herokuapp.com/v1/users/getMe';
+
+//JWT que identifica o usuário do login
+const jwtSessao = localStorage.getItem('jwt');
+
+const tarefasPendentes = querySelector('.tarefas-pendentes');
+
+
+
 function querySelector(el) {
     return document.querySelector(el)
 }
@@ -10,43 +23,44 @@ function empty(input) {
     return input.value.trim() === ''
 }
 
-const tarefasPendentes = querySelector('.tarefas-pendentes');
 
 // Efetua logoff da página de tarefas
 
 const btnFinalizaSessao = getById('closeApp');
-
 btnFinalizaSessao.addEventListener('click', () => window.location = 'index.html');
 
-//Variável para guardar tag de nome do usuário
-const nomeUsuario = querySelector('.user-info p');
 
-//link de integração com API - recuperar dados do usuário
-const urlGetMe = 'https://ctd-todo-api.herokuapp.com/v1/users/getMe';
-
-//JWT que identifica o usuário do login
-const jwtSessao = localStorage.getItem('jwt');
 
 window.addEventListener('load', function (evento) {
 
     evento.preventDefault();
 
-    const myHeaders = new Headers();
+    // const myHeaders = new Headers();
 
-    myHeaders.append('Content-Type', 'application/json');
-    myHeaders.append('Authorization', jwtSessao);
+    // myHeaders.append('Content-Type', 'application/json');
+    // myHeaders.append('Authorization', jwtSessao);
+
+    // const requestOptions = {
+    //     method: 'GET',
+    //     headers: myHeaders,
+    //     redirect: 'follow'
+    // }
 
     const requestOptions = {
         method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
+        headers: {
+            'content-type': 'application/json',
+            authorization: jwtSessao
+        },
+        // redirect: 'follow'
     }
 
     fetch(urlGetMe, requestOptions)
         .then(response => {
             if (!response.ok) {
                 console.log(response);
-                throw new Error(`Error! status: ${response.status}`);
+                throw response;
+                // throw new Error(`Error! status: ${response.status}`);
             } else {
                 return response.json();
             }
@@ -64,38 +78,48 @@ window.addEventListener('load', function (evento) {
 
 })
 
+// FUNÇÃO PARA LISTAR AS TAREFAS
+
 function listarTarefa() {
 
     const urlTarefa = 'https://ctd-todo-api.herokuapp.com/v1/tasks'
 
-    // const tarefa = getById('nome');
+    const tarefa = getById('nome');
 
-    // const dataCriada = getById('timestamp');
+    const dataCriada = getById('timestamp');
 
+    let skeletonRef = document.getElementById('skeleton');
 
+    const jwtSessao = this.localStorage.getItem("jwt")
 
-    let skeletonRef = getById('#skeleton');
+    // const myHeaders = new Headers();
 
-    const myHeaders = new Headers();
-
-    myHeaders.append('Content-Type', 'application/json');
-    myHeaders.append('Authorization', jwtSessao);
+    // myHeaders.append('Content-Type', 'application/json');
+    // myHeaders.append('Authorization', jwtSessao);
 
 
     const settings = {
         method: 'GET',
-        headers: myHeaders
+        // headers: myHeaders
+        headers: {
+            'content-type': 'application/json',
+            authorization: jwtSessao
+        }
     }
 
     fetch(urlTarefa, settings)
-        .then(responseTask => {
-            if (!responseTask.ok) {
-                console.log(response);
-                throw new Error(`Error! status: ${response.status}`);
-            } else {
-                return response.json();
-            }
-        })
+
+        .then((responseTask) => responseTask.json())
+        // .then(responseTask => {
+        //     if (!responseTask.ok) {
+        //         console.log(response);
+        //         throw responseTask
+        //         // throw new Error(`Error! status: ${response.status}`);
+        //     } else {
+        //         return responseTask.json();
+        //     }
+        // })
+
         .then(function (tasks) {
             // é necessário que o skeleton, que nada mais é que a parte do código sem os dados inseridos com template string, fiquem desabilitado
             //quando é recebido o response pela API
@@ -122,135 +146,92 @@ function listarTarefa() {
         })
 
         // neste console log validamos se ele está trazendo o nome do usuario corretamente do login que acessamos no JWT.
-
-
         .catch(err => {
             console.log(err);
             alert("Falha ao validar as tarefas do usuario!")
 
-
-        })
+        });
 
 }
 
+
+
+// FUNÇÃO PARA CRIAR AS TAREFAS
 const urlCriar = 'https://ctd-todo-api.herokuapp.com/v1/tasks';
-
-const configCriar = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': localStorage.getItem('jwt')
-    },
-    body: ''
-
-};
-
-function criarTarefa() {
-
-    fetch(urlCriar, configCriar)
-        .then(response => response.json())
-
-}
-
 const inputTarefa = document.getElementById('novaTarefa');
 const btnNovaTarefa = querySelector('.nova-tarefa button');
 
-btnNovaTarefa.addEventListener('click', function (ev) {
-
-    ev.preventDefault();
-
-    criarTarefa();
-
-})
-
-
-
-let configuracaoCriar = {
-
-    method: 'POST',
-    body: '',
-
-    headers: {
-
-        'Content-Type': 'application/json',
-        'Authorization': localStorage.getItem('jwtSessao')
-
-    }
-}
-
 
 function criarTarefa() {
 
-    const urlCriar = 'https://ctd-todo-api.herokuapp.com/v1/tasks'
-
-    configuracaoCriar.body = JSON.stringify({
-
-        description: inputTarefa.value,
+    //o nosso data se encarrega de pegar do input a descrição da tarefa, e inicia-la como completed false,
+    const data = {
+        description: novaTarefa.value,
         completed: false
-    })
+    };
+    console.log(data);
 
-    console.log(inputTarefa.value)
 
-    inputTarefa.value = ''
+    const settings = {
 
-    fetch(urlCriar, configuracaoCriar).then(
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            Authorization: jwtSessao
+        },
 
-        response => {
-            response.json().then(
+        body: JSON.stringify(data)
+    }
 
-                task => {
 
-                    console.log(task.description, task.completed);
-                    const dataFormatada = new Date(task.createdAt).toLocaleDateString(
-                        'pt-BR', {
 
-                        day: '2-digit',
-                        month: '2-digit',
-                        year: 'numeric'
-                    }
+    fetch(urlCriar, settings)
 
-                    )
+        .then(
+            response => {
+                response.json().then(
+                    task => {
+                        console.log(task.description, task.completed);
+                        const dataFormatada = new Date(task.createdAt).toLocaleDateString(
+                            'pt-BR', {
+                            day: '2-digit',
+                            month: '2-digit',
+                            year: 'numeric'
+                        }
 
-                    tarefasPendentes.innerHTML = `
+                        )
+                        tarefasPendentes.innerHTML = `
                         <li class="tarefa">
                             <div class="not-done"></div>
                             <div class="descricao">
                                 <p class="nome">${task.description}</p>
                                 <p class="timestamp">Criada em: ${dataFormatada}</p>
                             </div> `
-
-                })
-
-        }
-    )
-
+                    })
+            }
+        )
 }
 
+// Evento dispara Função criarTarefa() ao clicar no botao +
+btnNovaTarefa.addEventListener('click', function (ev) {
+
+    ev.preventDefault();
+    criarTarefa();
+
+})
 
 
 window.addEventListener('load', () => {
-
-
     // se o token recebido for undefined, ele deslogue o usuario.
-
-
     if (localStorage.getItem('jwt') === 'undefined') {
-
         alert('token indefinido!')
     } else {
 
         listarTarefa()
-
         btnNovaTarefa.addEventListener('click', event => {
-
             event.preventDefault()
-
             criarTarefa()
         })
-
-
-
     }
-
-
 })
+
